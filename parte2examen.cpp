382c968a11a290f263e7d88e8336e15e5a7b2359
sofivasa <sofivasa@myuax.com>
2023-11-09 11:52:40 +0100

    parte2-primercommit

---
+++ parte2examen.cpp
@@ -0,0 +1,20 @@
+//Bibliotecas
+#include<iostream>
+using namespace std;
+//Función principal
+int main(){
+    //Definir variables
+    int a,b;
+    //Solicitar numeros al usuario
+    cout<<"Escriba el primer numero:";
+    //Leer los numeros introducidos por el usuario
+    cin>>a;
+    cout<<"Escriba el segundo numero:";
+    cin>>b;
+    //Realizar la suma de los numeros introducidos
+    int suma=a+b;
+    //Imprimir resultado de la suma
+    cout<<"El resultado de la suma es:"<<suma<<endl;
+    return 0;
+
+}
